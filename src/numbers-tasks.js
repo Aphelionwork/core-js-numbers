c04a9b5810975645cf2e8fe85e6c0ec8eed2c470
Vitali Klepcha <aphelionwork@gmail.com>
2023-12-31 00:09:22 +0100

    feat: 11 tasks were done

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -91,8 +91,9 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  const x = (0 - b) / a;
+  return x;
 }
 
 /**
@@ -112,8 +113,14 @@ function getLinearEquationRoot(/* a, b */) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const sum = x1 * x2 + y1 * y2;
+  const square1 = Math.sqrt(x1 ** 2 + y1 ** 2);
+  const square2 = Math.sqrt(x2 ** 2 + y2 ** 2);
+  const mult = square1 * square2;
+  const cos = sum / mult;
+  const acos = Math.acos(cos);
+  return acos;
 }
 
 /**
@@ -129,8 +136,9 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  const result = value % 10;
+  return result;
 }
 
 /**
@@ -144,8 +152,9 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  const result = parseFloat(value);
+  return result;
 }
 
 /**
@@ -161,8 +170,9 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  const result = Math.sqrt(a ** 2 + b ** 2 + c ** 2);
+  return result;
 }
 
 /**
@@ -182,8 +192,10 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const degree = 10 ** pow;
+  const result = Math.round(num / degree) * degree;
+  return result;
 }
 
 /**
